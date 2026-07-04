fee887e73c64a02275a3af441f9fcdbac3fd2b68
peyman894-lgtm <peyman894@gmail.com>
2026-07-04 23:42:51 +0200

    Read Common Git repository

--- CryoAddon/MainForm.Designer.cs
+++ CryoAddon/MainForm.Designer.cs
@@ -1,40 +1,206 @@
 ﻿namespace CryoAddon
 {
+    
     partial class MainForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        private System.Windows.Forms.Label lblIniFile;
+        private System.Windows.Forms.TextBox txtIniFile;
+        private System.Windows.Forms.Button btnBrowseIni;
+        private System.Windows.Forms.Button btnLoadFiles;
+
+        private System.Windows.Forms.Label lblRepositoryTitle;
+        private System.Windows.Forms.Label lblRepositoryPath;
+
+        private System.Windows.Forms.DataGridView dgvSourceFiles;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn colSelect;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colFileName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colExtension;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colFullPath;
+
+        private System.Windows.Forms.Label lblLog;
+        private System.Windows.Forms.TextBox txtLog;
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
             {
                 components.Dispose();
             }
+
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblIniFile = new System.Windows.Forms.Label();
+            this.txtIniFile = new System.Windows.Forms.TextBox();
+            this.btnBrowseIni = new System.Windows.Forms.Button();
+            this.btnLoadFiles = new System.Windows.Forms.Button();
+            this.lblRepositoryTitle = new System.Windows.Forms.Label();
+            this.lblRepositoryPath = new System.Windows.Forms.Label();
+            this.dgvSourceFiles = new System.Windows.Forms.DataGridView();
+            this.colSelect = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.colFileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colExtension = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colFullPath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblLog = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSourceFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblIniFile
+            // 
+            this.lblIniFile.AutoSize = true;
+            this.lblIniFile.Location = new System.Drawing.Point(20, 22);
+            this.lblIniFile.Name = "lblIniFile";
+            this.lblIniFile.Size = new System.Drawing.Size(75, 16);
+            this.lblIniFile.TabIndex = 0;
+            this.lblIniFile.Text = "INI file path:";
+            // 
+            // txtIniFile
+            // 
+            this.txtIniFile.Location = new System.Drawing.Point(110, 19);
+            this.txtIniFile.Name = "txtIniFile";
+            this.txtIniFile.Size = new System.Drawing.Size(560, 22);
+            this.txtIniFile.TabIndex = 1;
+            // 
+            // btnBrowseIni
+            // 
+            this.btnBrowseIni.Location = new System.Drawing.Point(685, 17);
+            this.btnBrowseIni.Name = "btnBrowseIni";
+            this.btnBrowseIni.Size = new System.Drawing.Size(90, 27);
+            this.btnBrowseIni.TabIndex = 2;
+            this.btnBrowseIni.Text = "Browse";
+            this.btnBrowseIni.UseVisualStyleBackColor = true;
+            this.btnBrowseIni.Click += new System.EventHandler(this.btnBrowseIni_Click);
+            // 
+            // btnLoadFiles
+            // 
+            this.btnLoadFiles.Location = new System.Drawing.Point(790, 17);
+            this.btnLoadFiles.Name = "btnLoadFiles";
+            this.btnLoadFiles.Size = new System.Drawing.Size(120, 27);
+            this.btnLoadFiles.TabIndex = 3;
+            this.btnLoadFiles.Text = "Load Files";
+            this.btnLoadFiles.UseVisualStyleBackColor = true;
+            this.btnLoadFiles.Click += new System.EventHandler(this.btnLoadFiles_Click);
+            // 
+            // lblRepositoryTitle
+            // 
+            this.lblRepositoryTitle.AutoSize = true;
+            this.lblRepositoryTitle.Location = new System.Drawing.Point(20, 62);
+            this.lblRepositoryTitle.Name = "lblRepositoryTitle";
+            this.lblRepositoryTitle.Size = new System.Drawing.Size(105, 16);
+            this.lblRepositoryTitle.TabIndex = 4;
+            this.lblRepositoryTitle.Text = "Repository path:";
+            // 
+            // lblRepositoryPath
+            // 
+            this.lblRepositoryPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblRepositoryPath.Location = new System.Drawing.Point(140, 57);
+            this.lblRepositoryPath.Name = "lblRepositoryPath";
+            this.lblRepositoryPath.Size = new System.Drawing.Size(770, 27);
+            this.lblRepositoryPath.TabIndex = 5;
+            this.lblRepositoryPath.Text = "-";
+            this.lblRepositoryPath.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // dgvSourceFiles
+            // 
+            this.dgvSourceFiles.AllowUserToAddRows = false;
+            this.dgvSourceFiles.AllowUserToDeleteRows = false;
+            this.dgvSourceFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvSourceFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSourceFiles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colSelect,
+            this.colFileName,
+            this.colExtension,
+            this.colFullPath});
+            this.dgvSourceFiles.Location = new System.Drawing.Point(20, 100);
+            this.dgvSourceFiles.Name = "dgvSourceFiles";
+            this.dgvSourceFiles.RowHeadersWidth = 51;
+            this.dgvSourceFiles.RowTemplate.Height = 24;
+            this.dgvSourceFiles.Size = new System.Drawing.Size(890, 360);
+            this.dgvSourceFiles.TabIndex = 6;
+            // 
+            // colSelect
+            // 
+            this.colSelect.HeaderText = "Select";
+            this.colSelect.MinimumWidth = 6;
+            this.colSelect.Name = "colSelect";
+            this.colSelect.Width = 70;
+            // 
+            // colFileName
+            // 
+            this.colFileName.HeaderText = "File Name";
+            this.colFileName.MinimumWidth = 6;
+            this.colFileName.Name = "colFileName";
+            this.colFileName.ReadOnly = true;
+            this.colFileName.Width = 220;
+            // 
+            // colExtension
+            // 
+            this.colExtension.HeaderText = "Type";
+            this.colExtension.MinimumWidth = 6;
+            this.colExtension.Name = "colExtension";
+            this.colExtension.ReadOnly = true;
+            this.colExtension.Width = 80;
+            // 
+            // colFullPath
+            // 
+            this.colFullPath.HeaderText = "Full Path";
+            this.colFullPath.MinimumWidth = 6;
+            this.colFullPath.Name = "colFullPath";
+            this.colFullPath.ReadOnly = true;
+            this.colFullPath.Width = 480;
+            // 
+            // lblLog
+            // 
+            this.lblLog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblLog.AutoSize = true;
+            this.lblLog.Location = new System.Drawing.Point(20, 475);
+            this.lblLog.Name = "lblLog";
+            this.lblLog.Size = new System.Drawing.Size(33, 16);
+            this.lblLog.TabIndex = 7;
+            this.lblLog.Text = "Log:";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(20, 500);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(890, 120);
+            this.txtLog.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+            this.ClientSize = new System.Drawing.Size(935, 640);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.lblLog);
+            this.Controls.Add(this.dgvSourceFiles);
+            this.Controls.Add(this.lblRepositoryPath);
+            this.Controls.Add(this.lblRepositoryTitle);
+            this.Controls.Add(this.btnLoadFiles);
+            this.Controls.Add(this.btnBrowseIni);
+            this.Controls.Add(this.txtIniFile);
+            this.Controls.Add(this.lblIniFile);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TIA Source Updater";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSourceFiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
-        #endregion
+        }
     }
 }
+
 
--- CryoAddon/MainForm.cs
+++ CryoAddon/MainForm.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +13,279 @@ namespace CryoAddon
         public MainForm()
         {
             InitializeComponent();
+
+            txtIniFile.Text = Path.Combine(Application.StartupPath, "settings.ini");
         }
+
+        private void btnBrowseIni_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "INI files (*.ini)|*.ini|All files (*.*)|*.*";
+            dialog.Title = "Select settings.ini";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                txtIniFile.Text = dialog.FileName;
+            }
+        }
+
+        private async void btnLoadFiles_Click(object sender, EventArgs e)
+        {
+            dgvSourceFiles.Rows.Clear();
+            txtLog.Clear();
+            lblRepositoryPath.Text = "-";
+
+            string iniPath = txtIniFile.Text.Trim();
+
+            if (!File.Exists(iniPath))
+            {
+                Log("ERROR: settings.ini file not found.");
+                return;
+            }
+
+            string repositoryUrl = ReadValueFromIni(iniPath, "Common", "RepositoryUrl");
+
+            if (string.IsNullOrWhiteSpace(repositoryUrl))
+            {
+                Log("ERROR: RepositoryUrl not found in settings.ini.");
+                return;
+            }
+
+            lblRepositoryPath.Text = repositoryUrl;
+            Log("GitHub repository: " + repositoryUrl);
+
+            try
+            {
+                await LoadFilesFromGitHub(repositoryUrl);
+            }
+            catch (Exception ex)
+            {
+                Log("ERROR: " + ex.Message);
+            }
+        }
+
+        private async Task LoadFilesFromGitHub(string repositoryUrl)
+        {
+            GitHubRepositoryInfo repo = ParseGitHubRepositoryUrl(repositoryUrl);
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.UserAgent.ParseAdd("TiaSourceUpdater");
+
+                Log("Connection status: Connecting to GitHub...");
+
+                string repoApiUrl = "https://api.github.com/repos/" + repo.Owner + "/" + repo.Name;
+                string repoJson = await client.GetStringAsync(repoApiUrl);
+
+                Log("Connection status: OK");
+
+                string defaultBranch = GetJsonValue(repoJson, "default_branch");
+                string lastUpdatedTime = GetJsonValue(repoJson, "updated_at");
+
+                if (string.IsNullOrWhiteSpace(defaultBranch))
+                {
+                    defaultBranch = "main";
+                }
+
+                Log("Repository URL: " + repositoryUrl);
+                Log("Repository owner: " + repo.Owner);
+                Log("Repository name: " + repo.Name);
+                Log("Default branch: " + defaultBranch);
+
+                if (!string.IsNullOrWhiteSpace(lastUpdatedTime))
+                {
+                    Log("Last updated time: " + lastUpdatedTime);
+                }
+
+                string commitApiUrl = repoApiUrl + "/commits/" + defaultBranch;
+                string commitJson = await client.GetStringAsync(commitApiUrl);
+
+                string latestCommitSha = GetJsonValue(commitJson, "sha");
+                string latestCommitMessage = GetNestedCommitMessage(commitJson);
+                string latestCommitAuthor = GetNestedCommitAuthor(commitJson);
+
+                if (!string.IsNullOrWhiteSpace(latestCommitSha))
+                {
+                    Log("Latest commit SHA: " + latestCommitSha);
+                }
+
+                if (!string.IsNullOrWhiteSpace(latestCommitMessage))
+                {
+                    Log("Latest commit message: " + latestCommitMessage);
+                }
+
+                if (!string.IsNullOrWhiteSpace(latestCommitAuthor))
+                {
+                    Log("Latest commit author: " + latestCommitAuthor);
+                }
+
+                string treeApiUrl = repoApiUrl + "/git/trees/" + defaultBranch + "?recursive=1";
+                string treeJson = await client.GetStringAsync(treeApiUrl);
+
+                MatchCollection matches = Regex.Matches(
+                    treeJson,
+                    "\"path\"\\s*:\\s*\"([^\"]+)\"",
+                    RegexOptions.IgnoreCase
+                );
+
+                int totalCount = 0;
+                int sclCount = 0;
+                int dbCount = 0;
+                int udtCount = 0;
+
+                Log("File paths found:");
+
+                foreach (Match match in matches)
+                {
+                    string filePath = match.Groups[1].Value.Replace("\\/", "/");
+                    string extension = Path.GetExtension(filePath).ToLower();
+
+                    if (extension == ".db" || extension == ".scl" || extension == ".udt")
+                    {
+                        string rawUrl =
+                            "https://raw.githubusercontent.com/" +
+                            repo.Owner + "/" +
+                            repo.Name + "/" +
+                            defaultBranch + "/" +
+                            filePath;
+
+                        dgvSourceFiles.Rows.Add(
+                            false,
+                            Path.GetFileName(filePath),
+                            extension,
+                            rawUrl
+                        );
+
+                        Log("  " + filePath);
+
+                        totalCount++;
+
+                        if (extension == ".scl")
+                        {
+                            sclCount++;
+                        }
+                        else if (extension == ".db")
+                        {
+                            dbCount++;
+                        }
+                        else if (extension == ".udt")
+                        {
+                            udtCount++;
+                        }
+                    }
+                }
+
+                Log("Number of files found: " + totalCount);
+                Log("Number of .scl files: " + sclCount);
+                Log("Number of .db files: " + dbCount);
+                Log("Number of .udt files: " + udtCount);
+            }
+        }
+
+        private GitHubRepositoryInfo ParseGitHubRepositoryUrl(string repositoryUrl)
+        {
+            Uri uri = new Uri(repositoryUrl);
+
+            string[] parts = uri.AbsolutePath.Trim('/').Split('/');
+
+            if (parts.Length < 2)
+            {
+                throw new Exception("Invalid GitHub repository URL.");
+            }
+
+            return new GitHubRepositoryInfo
+            {
+                Owner = parts[0],
+                Name = parts[1].Replace(".git", "")
+            };
+        }
+
+        private string ReadValueFromIni(string iniPath, string sectionName, string keyName)
+        {
+            bool insideSection = false;
+
+            foreach (string rawLine in File.ReadAllLines(iniPath))
+            {
+                string line = rawLine.Trim();
+
+                if (line == "" || line.StartsWith(";") || line.StartsWith("#"))
+                {
+                    continue;
+                }
+
+                if (line.StartsWith("[") && line.EndsWith("]"))
+                {
+                    string currentSection = line.Trim('[', ']');
+                    insideSection = currentSection.Equals(sectionName, StringComparison.OrdinalIgnoreCase);
+                    continue;
+                }
+
+                if (insideSection && line.StartsWith(keyName + "=", StringComparison.OrdinalIgnoreCase))
+                {
+                    return line.Substring((keyName + "=").Length).Trim();
+                }
+            }
+
+            return "";
+        }
+
+        private string GetJsonValue(string json, string key)
+        {
+            Match match = Regex.Match(
+                json,
+                "\"" + key + "\"\\s*:\\s*\"([^\"]+)\"",
+                RegexOptions.IgnoreCase
+            );
+
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+
+            return "";
+        }
+
+        private string GetNestedCommitMessage(string json)
+        {
+            Match match = Regex.Match(
+                json,
+                "\"message\"\\s*:\\s*\"([^\"]+)\"",
+                RegexOptions.IgnoreCase
+            );
+
+            if (match.Success)
+            {
+                return match.Groups[1].Value.Replace("\\n", " ");
+            }
+
+            return "";
+        }
+
+        private string GetNestedCommitAuthor(string json)
+        {
+            Match match = Regex.Match(
+                json,
+                "\"author\"\\s*:\\s*\\{[^\\}]*\"name\"\\s*:\\s*\"([^\"]+)\"",
+                RegexOptions.IgnoreCase
+            );
+
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+
+            return "";
+        }
+
+        private void Log(string message)
+        {
+            txtLog.AppendText(DateTime.Now.ToString("HH:mm:ss") + "  " + message + Environment.NewLine);
+        }
+    }
+
+    public class GitHubRepositoryInfo
+    {
+        public string Owner { get; set; }
+        public string Name { get; set; }
     }
 }
